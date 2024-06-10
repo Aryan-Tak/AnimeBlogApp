ae67d6d276d4ba367913d7af5810b649251c145f
Aryan-Tak <imaryan7781@gmail.com>
2024-06-10 19:13:40 +0530

    Configure Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-
-
+import React ,{ useState ,useEffect} from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login , logout} from "./store/authSlice"
+import {Footer , Header} from './components'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [ loading , setLoading]= useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1 className='bg-yellow-300 p-2'>Test1</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((useData) => {
+      if (useData) {
+        dispatch(login({useData}))
+        
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  } , [])
+
+  return !loading ? (
+    <div className='bg-slate-300 min-h-screen flex flex-wrap'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
