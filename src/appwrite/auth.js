4bcc211b7a3c5660926eb85fc4698af3b4c6db57
Aryan-Tak <imaryan7781@gmail.com>
2024-06-13 19:01:21 +0530

    Tech Done

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,6 +1,6 @@
-import conf from "../conf/conf";
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
 
-import {Client , Account , ID} from "appwrite"
 
 export class AuthService {
     client = new Client();
@@ -10,26 +10,27 @@ export class AuthService {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
-        this.account = new Account(this.Client);
+        this.account = new Account(this.client);
+            
     }
 
-    async createAccount({email , password , name}) {
+    async createAccount({email, password, name}) {
         try {
-            const userAccount = await this.account.create(ID.unique() , email , password , name);
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
             if (userAccount) {
-                // call another function
-                return this.login({email , password});
+                // call another method
+                return this.login({email, password});
             } else {
-               return userAccount; 
+               return  userAccount;
             }
         } catch (error) {
             throw error;
         }
     }
 
-    async login({email , password}) {
+    async login({email, password}) {
         try {
-            return await this.account.createEmailSession(email , password);
+            return await this.account.createEmailSession(email, password);
         } catch (error) {
             throw error;
         }
@@ -37,25 +38,25 @@ export class AuthService {
 
     async getCurrentUser() {
         try {
-           return await this.account.get(); 
+            return await this.account.get();
         } catch (error) {
-            throw error;
-            
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
 
         return null;
     }
 
     async logout() {
+
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            throw error;
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
-
 }
 
 const authService = new AuthService();
 
-export default authService;
+export default authService
+
--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,13 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import PostForm from "./post-form/PostForm";
 export {
     Header,
     Footer,
@@ -12,6 +19,13 @@ export {
     Logo,
     LogoutBtn,
     Button,
-    Input
+    Input,
+    RTE,
+    Select,
+    Signup,
+    Login,
+    PostCard,
+    AuthLayout,
+    PostForm
 
 }
